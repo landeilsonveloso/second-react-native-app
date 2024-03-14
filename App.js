d0bf5e8e994b5c8028ecaa63dc590ea60803c1fd
landeilsonveloso <landeilsonveloso@gmail.com>
2024-03-14 20:27:34 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { Button, Image, Text, TextInput, View } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [name, setname] = useState("")
+  const [visibleName, setVisibleName] = useState(name)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <View style={{flex: 1, backgroundColor: '#fff'}}>
+      <View style={{flex: 1, justifyContent: "space-evenly", alignItems: "center", backgroundColor: "#fff"}}>
+        <View style={{alignItems: "center"}}>
+          <Text style={{fontSize: 16}}>
+            Cadastre seu Projeto:
+          </Text>
+        </View>
+
+        <View style={{flexDirection: "row", alignItems: "center", marginTop: 20}}>
+          <Text>
+            Nome:
+          </Text>
+
+          <TextInput style={{width: 250, height: 30, borderWidth: 1, marginLeft: 10, padding: 5}} value={name} onChangeText={newName => setname(newName)}/>
+        </View>
+
+        <View style={{width: 300, flexDirection: "row", justifyContent: "space-between"}}>
+          <View>
+            <Button title="Limpar" onPress={() => setname("")}/>
+          </View>
+
+          <View>
+            <Button title="Cadastrar" onPress={() => setVisibleName(name)}/>
+          </View>
+        </View>
+      </View>
+
+      <View style={{flex: 1, justifyContent: "space-evenly", alignItems: "center"}}>
+        <Text>
+          {visibleName}
+        </Text>
+
+        <Image style={{width: 250, height: 250}} source={require("./assets/meeting.jpg")}/>
+      </View>
+    </View>
+  )
+}
